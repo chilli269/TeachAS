4e020be0159ca7016ccfa547ea55f4ba280579b7
Mihnea <62653994+chilli269@users.noreply.github.com>
2022-09-05 16:31:39 +0300

    added meeting length field

--- extra_addons/teachas/teachas_module/models/teachas.py
+++ extra_addons/teachas/teachas_module/models/teachas.py
@@ -4,9 +4,10 @@ class TeachAS(models.Model):
     _name = "teachas"
 
     materie = fields.Selection([('informatica', 'Informatică'), ('matematica', 'Matematică'), ('fizica', 'Fizică'), ('biologie', 'Biologie'), ('chimie', 'Chimie'), ('engleza', 'Engleză'),
-    ('franceza', 'Franceză'), ('germana', 'Germană'), ('spaniola', 'Spaniolă'), ('latina', 'Latină'), ('romana', 'Română'), ('istorie', 'Istorie'), ('geografie', 'Geografie')], string="Materie")
-    mentor = fields.Many2one('res.partner', string="Mentor")
-    elev = fields.Many2one('res.partner', string="Elev")
-    data = fields.Datetime(string="Meeting Date and Time")
+    ('franceza', 'Franceză'), ('germana', 'Germană'), ('spaniola', 'Spaniolă'), ('latina', 'Latină'), ('romana', 'Română'), ('istorie', 'Istorie'), ('geografie', 'Geografie')], string="Materie", required=True)
+    mentor = fields.Many2one('res.partner', string="Mentor", required=True)
+    elev = fields.Many2one('res.partner', string="Elev", required=True)
+    data = fields.Datetime(string="Meeting Date and Time", required=True)
+    time_length = fields.Float(string = "Meeting Length(hours)", required=True)
     
 
