820e4dcac1a8dad7ebbf39839886c6c06c34f400
dennisbart <dennibart@gmail.com>
2022-09-06 12:41:22 +0300

    add

---
+++ extra_addons/teachas/teachas_website/controllers/main.py
@@ -0,0 +1,14 @@
+from odoo import fields, http
+from odoo.http import request
+
+class TeachasController(http.Controller):
+
+      @http.route('/dashboard', type='http',auth="public", website=True)
+      def dashboard(self):
+
+            current_uid=http.request.env.context.get('uid')
+            contact=request.env['res.partner'].browse(current_uid)
+            
+            return http.request.render('teachas_website.dashboard',{
+               'type':contact.contact_type   
+            })
