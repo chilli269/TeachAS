527d833753a839d3d160d619f8865956d979a123
dennisbart <dennibart@gmail.com>
2022-09-06 12:51:09 +0300

    add_2

--- extra_addons/teachas/teachas_website/controllers/main.py
+++ extra_addons/teachas/teachas_website/controllers/main.py
@@ -6,9 +6,9 @@ class TeachasController(http.Controller):
       @http.route('/dashboard', type='http',auth="public", website=True)
       def dashboard(self):
 
-            current_uid=http.request.env.context.get('uid')
-            contact=request.env['res.user'].browse(current_uid)
+            # current_uid=http.request.env.context.get('uid')
+            contact=request.env['res.users'].browse(http.request.env.context.get('uid'))
             
             return http.request.render('teachas_website.dashboard',{
-               'type':contact.contact_type   
+               'contact_type':contact.contact_type   
             })
