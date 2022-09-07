0556d371e4bc8ebf49dc52b3a5e6c6aa4f93e957
Mihnea <62653994+chilli269@users.noreply.github.com>
2022-09-07 17:57:11 +0300

    sessions in controller work

--- extra_addons/teachas/teachas_website/controllers/main.py
+++ extra_addons/teachas/teachas_website/controllers/main.py
@@ -5,12 +5,8 @@ class TeachasController(http.Controller):
 
       @http.route('/dashboard', type='http',auth="public", website=True)
       def dashboard(self):
-
-            # current_uid=http.request.env.context.get('uid')
             user_id=request.env['res.users'].browse(http.request.env.context.get('uid'))
-            print(user_id)
-            sessions=request.env['teachas'].sudo().search([('elev.id', '=', user_id.id)])
-            print(sessions)
+            sessions=request.env['teachas'].sudo().search(['|', ('elev.id', '=', user_id.id), ('mentor.id', '=', user_id.id)])
             return http.request.render('teachas_website.dashboard',{
                'sessions':sessions
             })
