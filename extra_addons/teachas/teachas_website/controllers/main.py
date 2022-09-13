f82aa84afce2ffdeb3426be0971070c088d06b69
Mihnea <62653994+chilli269@users.noreply.github.com>
2022-09-13 17:52:20 +0300

    added subject selection on schedule-meeting page

--- extra_addons/teachas/teachas_website/controllers/main.py
+++ extra_addons/teachas/teachas_website/controllers/main.py
@@ -7,21 +7,24 @@ class TeachasController(http.Controller):
       def dashboard(self):
             user_id=request.env['res.users'].browse(http.request.env.context.get('uid'))
             sessions=request.env['teachas'].sudo().search(['|', ('elev.id', '=', user_id.id), ('mentor.id', '=', user_id.id)])
-            subjects=request.env['teachas.subjects'].sudo().search([])
             return http.request.render('teachas_website.dashboard',{
                'sessions':sessions,
-               'subjects':subjects
             })
 
 
       @http.route('/schedule-meeting', type='http', auth='public', website=True)
       def schedule_meeting(self):
-            return http.request.render('teachas_website.schedule_meeting')
+            subjects=request.env['teachas.subjects'].sudo().search([])
+            return http.request.render('teachas_website.schedule_meeting', {
+                  'subjects':subjects
+            })
 
       @http.route(['/schedule-meeting/submit'], type='http', auth="public", website=True)
       def customer_form_submit(self, **post):
             partner = request.env['teachas'].sudo().create({
-                  'time_length': post.get('time_length'),
+                  'hour_length': post.get('hour_length'),
+                  'minute_length': post.get('minute_length'),
+                  'materie': post.get('subject')
             })
             vals = {
                   'partner': partner,
