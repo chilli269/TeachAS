b79d9b9bcca2c13070b76b8c6f6d0566bda50c53
Mihnea <62653994+chilli269@users.noreply.github.com>
2022-09-19 09:05:51 +0300

    added details and session_type fields

--- extra_addons/teachas/teachas_module/models/teachas.py
+++ extra_addons/teachas/teachas_module/models/teachas.py
@@ -7,12 +7,14 @@ class TeachAS(models.Model):
     materie = fields.Many2one('teachas.subjects', string="Materie", required=True)
     mentor = fields.Many2one('res.users', string="Mentor")
     elev = fields.Many2one('res.users', string="Elev")
-    data = fields.Datetime(string="Meeting Date and Time")
+    data = fields.Date(string="Meeting Date and Time")
     # time_length = fields.Float(string = "Meeting Length(hours)")
     time_length=fields.Selection([('half','30 Minutes'),('hour','One Hour'),('hourhalf','One Hour and 30 Minutes'),('twohours','Two Hours')],'Meeting duration', required=True)
     # hour_length=fields.Integer(string="Hours")
     # minute_length=fields.Integer(string="Minutes")
     is_session=fields.Boolean('Is Interactive Session?', default=False)
+    session_type=fields.Selection([('online', 'Online'),('fizic','Fizic'),('other','I prefer not to say')], string="Session Type")
+    details=fields.Text(string="Meeting Details")
 
     # @api.onchange('minute_length')
     # def _check_minutes(self):
