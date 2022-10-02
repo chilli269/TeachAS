a2a745badb66d7be99378d8772e697012339df40
dennisbart <dennibart@gmail.com>
2022-10-02 20:06:09 +0300

    adaugat_grade

--- extra_addons/teachas/teachas_module/models/res_users.py
+++ extra_addons/teachas/teachas_module/models/res_users.py
@@ -9,3 +9,4 @@ class AddContactType(models.Model):
     available_hours = fields.Float(string="Available Weekly Hours")
     phone_number = fields.Char(string="Phone Number")
     exp_points = fields.Float('Experience points', default=0) #readonly=True,need to make it readonly after calculation is implemented
+    grade_id = fields.Selection([('8','5th-8th'),('9', '9th'),('10', '10th'),('11', '11th'),('12','12th')], string="Grade")
