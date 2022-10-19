7e5ccbce1552585ecd3d46b19adcc4f4634872cb
dennisbart <dennibart@gmail.com>
2022-10-19 18:13:56 +0300

    interactive_sessions

--- extra_addons/teachas/teachas_website/static/src/js/teachas_snippet.js
+++ extra_addons/teachas/teachas_website/static/src/js/teachas_snippet.js
@@ -1,19 +1,20 @@
 odoo.define('teachas_website.interactive_sessions_snippet',function(require){
 
       const PublicWidget=require('web.public.widget');
-      var rpc = require('web.rpc');
+      // var rpc = require('web.rpc');
 
       // const DynamicSnippetCarousel=require('website.s_dynamic_snippet_carousel');
 
       var Dynamic = PublicWidget.Widget.extend({
             selector:'.js_interactive_sessions_snippet',
+            
             start:function() {
-                  var self=this;
-                  rpc.query({
-                        route:'/custom_snippets/total_interactive_sessions',
+                  console.log('A INTRAT IN START');
+                  this._rpc({
+                        route:'/custom_snippets/interactive_sessions',
                   }).then(function(result){
                         console.log(result);
-                        self.$el.find('#find_me_please').append(...$(result));
+                        $('#find_me_please').append(...$(result));
                   });
             },
       });
